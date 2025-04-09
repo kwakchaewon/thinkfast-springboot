1089e42e51e54254b90bfe5d5116ccc7e3da363a
kwakchaewon <74225015+kwakchaewon@users.noreply.github.com>
2025-04-09 11:52:36 +0900

    [chore] init commit

---
+++ src/test/java/com/example/thinkfast/ThinkfastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thinkfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkfastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
