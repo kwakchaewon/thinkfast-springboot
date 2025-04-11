e877bdad4ecee6e266fdd23fed0f0a03949462a4
kwakchaewon <ksh03003@naver.com>
2025-04-11 11:35:48 +0900

    [fix] ApiResponseBody 포맷에 맞게 전달되도록 수정

--- src/main/java/com/example/thinkfast/controller/TestController.java
+++ src/main/java/com/example/thinkfast/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.example.thinkfast.controller;
+
+import com.example.thinkfast.common.ApiResponse;
+import com.example.thinkfast.common.ApiResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@ApiResponseBody
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
 
     @GetMapping
-    public String test() {
-        return "성공";
+    public ApiResponse<String> test() {
+        return ApiResponse.success("성공");
     }
 } 
