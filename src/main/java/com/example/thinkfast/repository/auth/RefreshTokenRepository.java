58257a8af07eebcae89e3b054294efd9ff940441
kwakchaewon <ksh03003@naver.com>
2025-04-11 13:04:27 +0900

    [chore] 누락된 어노테이션 추가

--- src/main/java/com/example/thinkfast/repository/auth/RefreshTokenRepository.java
+++ src/main/java/com/example/thinkfast/repository/auth/RefreshTokenRepository.java
@@ -3,9 +3,11 @@ package com.example.thinkfast.repository.auth;
 import com.example.thinkfast.domain.auth.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
     Optional<RefreshToken> findByUsername(String username);
