efd18a2a006eab2d5f36d3fe8756c89c90c4b045
kwakchaewon <ksh03003@naver.com>
2025-04-19 20:43:52 +0900

    [fix] native query 필드명 지정

--- src/main/java/com/example/thinkfast/repository/auth/UserRepository.java
+++ src/main/java/com/example/thinkfast/repository/auth/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.thinkfast.repository.auth;
 import com.example.thinkfast.domain.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,5 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     @Query("SELECT u.id FROM User u WHERE u.username = :username")
-    Long findIdByUsername(String username);
+    Long findIdByUsername(@Param("username") String username);
 }
