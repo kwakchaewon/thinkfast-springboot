16533558d9e3ebdd4edf60b57847bb1accb48c96
kwakchaewon <ksh03003@naver.com>
2025-04-19 20:28:46 +0900

    [fix] 유저 id 조회 시 id 필드만 조회하도록 변경

--- src/main/java/com/example/thinkfast/repository/auth/UserRepository.java
+++ src/main/java/com/example/thinkfast/repository/auth/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.thinkfast.repository.auth;
 
 import com.example.thinkfast.domain.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
+
+    @Query("SELECT u.id FROM User u WHERE u.username = :username")
     Long findIdByUsername(String username);
 }
