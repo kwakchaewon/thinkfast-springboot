51132eb2ad296a60f80c6212380daa3800b7c283
kwakchaewon <ksh03003@naver.com>
2025-05-01 19:37:31 +0900

    [fix] 만료된 토큰 및 Exception 분기 시 401 반환하도록 로직 추가

--- src/main/java/com/example/thinkfast/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/thinkfast/security/JwtAuthenticationFilter.java
@@ -6,26 +6,37 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+            throws IOException {
         String token = jwtTokenProvider.resolveToken(request);
 
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        try {
+            if (token != null) {
+                if (jwtTokenProvider.validateToken(token)) {
+                    Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    // 유효하지 않는 토큰 (ex: 만료된 토큰) 401 반환
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write("Invalid JWT token");
+                    return;
+                }
+            }
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            // 예외 상황 처리 로직
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Authentication error: " + e.getMessage());
         }
-
-        filterChain.doFilter(request, response);
     }
+
 } 
