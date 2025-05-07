1418ac8f1966631fbbea28658b1e9c0c42097e82
kwakchaewon <ksh03003@naver.com>
2025-05-07 17:20:47 +0900

    [feat] 새로운 응답 생성 시 Notifications 테이블에 저장

--- src/main/java/com/example/thinkfast/domain/Notification.java
+++ src/main/java/com/example/thinkfast/domain/Notification.java
@@ -1,17 +1,16 @@
 package com.example.thinkfast.domain;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
@@ -20,17 +19,32 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "Notification")
+@Table(name = "NOTIFICATIONS")
 @Setter
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String type; // 알람 유형 (예: SURVEY_RESPONSE)
-    private Long recipentId; // 수신자 (회원 ID)
+    private Long recipientId; // 수신자 (회원 ID)
     private String message; // 알람 내용
     private Long referenceId; // 연관된 설문 ID 등
     private Boolean isRead; // 읽음 여부
     private LocalDateTime createdAt; // 생성일
     private LocalDateTime expiresAt; // 만료일
+
+    @PrePersist
+    protected void onCreate() {
+        if (isRead == null){
+            isRead = false;
+        }
+
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+
+        if (expiresAt == null) {
+            expiresAt = createdAt.plusDays(7);
+        }
+    }
 }
--- src/main/java/com/example/thinkfast/realtime/RedisPublisher.java
+++ src/main/java/com/example/thinkfast/realtime/RedisPublisher.java
@@ -1,5 +1,7 @@
 package com.example.thinkfast.realtime;
 
+import com.example.thinkfast.domain.Notification;
+import com.example.thinkfast.repository.NotificationRepository;
 import com.example.thinkfast.repository.auth.UserRepository;
 import com.example.thinkfast.repository.survey.SurveyRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,6 +10,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class RedisPublisher {
@@ -15,6 +19,7 @@ public class RedisPublisher {
     private final ObjectMapper objectMapper;
     private final SurveyRepository surveyRepository;
     private final UserRepository userRepository;
+    private final NotificationRepository notificationRepository;
 
     private final String CHANNEL = "alarm-channel";
 
@@ -26,10 +31,18 @@ public class RedisPublisher {
         Long userId = surveyRepository.findUserIdById(surveyId);
         String username = userRepository.findUsernameById(userId);
 
+        Notification notification = Notification.builder()
+                .type("SURVEY_RESPONSE")
+                .recipientId(userId)
+                .message("새 응답이 도착했습니다.")
+                .referenceId(surveyId)
+                .build();
+
+        Notification createdNotification = notificationRepository.save(notification);
+
         try {
-            AlarmMessage alarmMessage = new AlarmMessage(username, "새 응답이 도착했습니다.");
+            AlarmMessage alarmMessage = new AlarmMessage(username, createdNotification.getMessage(), createdNotification.getCreatedAt());
             String json = objectMapper.writeValueAsString(alarmMessage);
-            
             // 레디스 채널에 매시지 전달
             redisTemplate.convertAndSend(CHANNEL, json);
         } catch (Exception e) {
