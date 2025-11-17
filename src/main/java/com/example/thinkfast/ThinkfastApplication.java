88075e4aaf3e8a1b93a5615044424aab0a630dd6
kwakchaewon <ksh03003@naver.com>
2025-11-17 19:39:24 +0900

    [chore] Hibernate가 엔티티를 정확히 인식하도록 명시

--- src/main/java/com/example/thinkfast/ThinkfastApplication.java
+++ src/main/java/com/example/thinkfast/ThinkfastApplication.java
@@ -2,10 +2,12 @@ package com.example.thinkfast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EntityScan("com.example.thinkfast.domain")
 public class ThinkfastApplication {
 
 	public static void main(String[] args) {
