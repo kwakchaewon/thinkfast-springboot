9a51b04376818288490406466b656b9b5508cce1
kwakchaewon <ksh03003@naver.com>
2025-11-21 14:55:51 +0900

    [chore] UserDetailImpl.getAuthorities() null 반환 문제 수정
    권한 기반 접근 제어가 작동하지 않던 문제를 수정.
    getAuthorities() 메서드가 사용자의 Role에 맞는 GrantedAuthority를 반환.

--- src/main/java/com/example/thinkfast/security/UserDetailImpl.java
+++ src/main/java/com/example/thinkfast/security/UserDetailImpl.java
@@ -3,9 +3,11 @@ package com.example.thinkfast.security;
 import com.example.thinkfast.domain.auth.Role;
 import com.example.thinkfast.domain.auth.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class UserDetailImpl implements UserDetails {
     private String username;
@@ -15,7 +17,7 @@ public class UserDetailImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + this.roles.name()));
     }
 
     @Override
