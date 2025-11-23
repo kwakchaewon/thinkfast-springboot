c5d943ddfde57d8b1f3a8e57de3200f82849104c
kwakchaewon <ksh03003@naver.com>
2025-11-23 20:02:08 +0900

    [fix] 운영 서버 WebClientConfig 스캔 문제 해결

--- src/main/java/com/example/thinkfast/ThinkfastApplication.java
+++ src/main/java/com/example/thinkfast/ThinkfastApplication.java
@@ -1,9 +1,11 @@
 package com.example.thinkfast;
 
+import com.example.thinkfast.common.config.WebClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EntityScan("com.example.thinkfast.domain")
 @ComponentScan(basePackages = "com.example.thinkfast")
+@Import(WebClientConfig.class)
 public class ThinkfastApplication {
 
 	public static void main(String[] args) {
