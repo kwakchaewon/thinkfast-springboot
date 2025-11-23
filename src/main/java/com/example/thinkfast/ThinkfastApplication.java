ea608106e003f8d0eb57a8745f9f851d7e1b1402
kwakchaewon <ksh03003@naver.com>
2025-11-23 20:09:37 +0900

    [fix] 컴파일 타임에 클래스를 찾지 못해 발생한 빌드 오류 해결

--- src/main/java/com/example/thinkfast/ThinkfastApplication.java
+++ src/main/java/com/example/thinkfast/ThinkfastApplication.java
@@ -1,11 +1,9 @@
 package com.example.thinkfast;
 
-import com.example.thinkfast.common.config.WebClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -14,7 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EntityScan("com.example.thinkfast.domain")
 @ComponentScan(basePackages = "com.example.thinkfast")
-@Import(WebClientConfig.class)
 public class ThinkfastApplication {
 
 	public static void main(String[] args) {
